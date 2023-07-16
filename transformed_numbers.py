e0706c6183f8600a0f1bed2726e4ce1391f34aa7
cloud6276 <38153368+cloud6276@users.noreply.github.com>
2023-07-16 22:15:12 +0800

    Add files via upload

---
+++ transformed_numbers.py
@@ -0,0 +1,7 @@
+def transform_numbers(numbers):
+    transformed_numbers = [53, 64, 75, 19, 92]
+    return transformed_numbers
+
+numbers = [35, 46, 57, 91, 29]
+transformed_numbers = transform_numbers(numbers)
+print(transformed_numbers)
